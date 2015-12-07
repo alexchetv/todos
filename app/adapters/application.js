db15b9dc78e7a0fa731cf1a1cd77b54a7c5b05d5
alexchetv <alexchetv@gmail.com>
2015-12-07 18:17:23 +0500

    add live sync

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -13,25 +13,32 @@ export default Adapter.extend({
 	// Ember Data 2.0 Reload behavior
 	shouldReloadRecord: function() { return true; },
 	shouldReloadAll: function() { return true; },
-	shouldBackgroundReloadRecord: function() { return true; },
-	shouldBackgroundReloadAll: function() { return true; },
-	/*
+	shouldBackgroundReloadRecord: function() { return false; },
+	shouldBackgroundReloadAll: function() { return false; },
+
 	// Change watcher for ember-data
-	immediatelyLoadAllChangedRecords: function() {
+	LoadChangedUnloadDeletedRecord: function() {
 		this.db.changes({
 			since: 'now',
 			live: true,
 			returnDocs: false
 		}).on('change', function (change) {
+			//console.log('change',change);
 			var obj = this.db.rel.parseDocID(change.id);
 			// skip changes for non-relational_pouch docs. E.g., design docs.
 			if (!obj.type || obj.type === '') { return; }
 
 			//var appController = this.container.lookup("controller:application");
 			//appController.send('kickSpin');
-
-			var store = this.container.lookup('store:main');
-			store.find(obj.type);
+			if (change.deleted) {
+				var record = this.store.peekRecord(obj.type,obj.id);
+				if (record && !record.get("isDeleted")) {
+					console.log('record',record);
+					record.unloadRecord();
+				}
+			} else {
+				this.store.findRecord(obj.type,obj.id);
+			}
 		}.bind(this));
-	}.on('init')*/
+	}.on('init')
 });
