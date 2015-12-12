b8a1f80b57891e7b688c78ee699c1f7e5a9f8c17
alexchetv <alexchetv@gmail.com>
2015-12-12 19:47:06 +0500

    move logout method to login route

--- app/components/log-out.js
+++ app/components/log-out.js
@@ -1,20 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	classNames: ['user-info'],
-	store: Ember.inject.service(),
-	actions: {
-		logout() {
-			self = this;
-			let appAdapter = this.get('store').adapterFor('application');
-			appAdapter.remote.logout()
-				.then(function (info) {
-					console.log('logout',info);
-					self.sendAction('goToRoute', 'login');
-				}).catch(function (err) {
-					console.log('logout error', err);
-				});
-		}
-	}
-
+	classNames: ['user-info']
 });
--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	beforeModel:function(transition) {
+		self = this;
+		let appAdapter = this.get('store').adapterFor('application');
+		appAdapter.remote.logout()
+			.then(function (info) {
+				console.log('logout',info);
+			}).catch(function (err) {
+				console.log('logout error', err);
+			});
+	},
+	setupController: function(controller, model) {
+		this._super(controller, model);
+		this.controllerFor('application').setProperties({'currentUser': null,"currentPageTitle": "please log in","currentPageFooter":	""});
+	},
 });
