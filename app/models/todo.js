29198d689f3083c61bcb1331cad43d7681896d2a
alexchetv <alexchetv@gmail.com>
2015-12-13 09:43:55 +0500

    add author field

--- app/models/todo.js
+++ app/models/todo.js
@@ -3,5 +3,6 @@ import { Model } from 'ember-pouch';
 
 export default Model.extend({
 	title: DS.attr('string'),
-	completed: DS.attr('boolean')
+	completed: DS.attr('boolean'),
+	author: DS.attr('string')
 });
