a84f55f92d8ec7feae28d8e66804e0da35b67389
alexchetv <alexchetv@gmail.com>
2015-12-13 11:23:13 +0500

    change field name -- author on createdBy

--- app/models/todo.js
+++ app/models/todo.js
@@ -4,5 +4,5 @@ import { Model } from 'ember-pouch';
 export default Model.extend({
 	title: DS.attr('string'),
 	completed: DS.attr('boolean'),
-	author: DS.attr('string')
+	createdBy: DS.attr('string')
 });
