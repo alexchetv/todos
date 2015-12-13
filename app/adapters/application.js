084eb4b147537a4123bfe7296e21c4fde58b8c9d
alexchetv <alexchetv@gmail.com>
2015-12-13 09:40:47 +0500

    alert on Replication Denied

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,7 +4,6 @@ import { Adapter } from 'ember-pouch';
 //PouchDB.debug.enable('*');
 PouchDB.debug.disable();
 
-
 var db = new PouchDB(config.local_couch || 'todos');
 db.setMaxListeners(20);
 var remote = new PouchDB(config.remote_couch, {
@@ -15,5 +14,10 @@ remote.setMaxListeners(20);
 
 export default Adapter.extend({
 	db: db,
-	remote: remote
+	remote: remote,
+	replicationDenied: function(err){
+		console.log("ReplicationDenied",err);
+		alert(err.err.name+" : "+ err.err.reason + "\nSynchronization stopped!\nRollup last changes and reload page!");
+		this.stopReplication();
+	}.on("ReplicationDenied"),
 });
