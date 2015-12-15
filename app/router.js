626b226989870e8690d2ccfb9d2d3a09c06da9fa
alexchetv <alexchetv@gmail.com>
2015-12-15 05:29:15 +0500

    add routes and components concerning users

--- app/router.js
+++ app/router.js
@@ -11,6 +11,7 @@ Router.map(function() {
     this.route('incompleted');
   });
   this.route('login');
+  this.route('users');
 });
 
 export default Router;
---
+++ app/routes/users/slaves.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	model() {
+	return this.store.filter('user', function(user) {
+		return (user.roles.indexOf('_admin') == -1);
+	});
+},
+renderTemplate(controller, model) {
+	this.render('user.index', {
+		model: model
+	});
+}
+});
