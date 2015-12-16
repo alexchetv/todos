c8bfe61195e72c19dab5ffad95fdecb45229a3ae
alexchetv <alexchetv@gmail.com>
2015-12-16 05:04:50 +0500

    fix

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	beforeModel:function(transition) {
-		self = this;
+		var self = this;
 		let appAdapter = this.get('store').adapterFor('application');
 		appAdapter.remote.logout()
 			.then(function (info) {
